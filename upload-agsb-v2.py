5e5ffd5d070202a3b70458ea7ab23f1381cd39bc
bvvbjvo <122722751+bvvbjvo@users.noreply.github.com>
2025-08-08 11:09:50 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytdlala"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "lixio"         # 用户名
+UUID = "57751068-cef0-4241-b231-4eeb2e2b4797"                     # UUID，留空则自动生成
+PORT = 443                   # Vmess端口，留空或0则自动生成
+DOMAIN = "lixio.lixio.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiOTFmYzI1ZDViM2IxODU0OGM4YTA5MmNjYWNlMzU1ZmQiLCJ0IjoiNTdiYzlmMTMtNmVmMy00N2JhLWE5M2QtNWM3YjFlN2QxYzFhIiwicyI6Ik5UUmhNamt6WVdNdE9XTTJPQzAwTmpFMUxUazRZamd0WTJJeU9ETTBaV1ExTUdZMCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
